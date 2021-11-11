bbb5a6729e86da1721c5bdbf02495d0424973d6e
nguyendangkhanhduy <94118283+KhanhDuy0209@users.noreply.github.com>
2021-11-11 21:19:20 +0700

    Add files via upload

---
+++ js/dangky.js
@@ -0,0 +1,13 @@
+function show2()
+{
+    var str=confirm("Mua Hàng Thành Công!");
+    var str1=confirm("Bạn Có Muốn Xem Thêm Sản Phẩm Không!");
+    if(str1)
+    {
+        window.location.href="index.html";
+    }
+    else
+    {
+     
+    }
+}
